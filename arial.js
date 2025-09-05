2d1601f202420ab6e08e7af40abd5e4bd70e8a93
Ogura-Yui <ogura.y0412@gmail.com>
2025-09-05 16:56:06 +0900

    Update arial.js

--- arial.js
+++ arial.js
@@ -1,24 +1,21 @@
-window.addEventListener("DOMContentLoaded", () => {
-  // 星を表示するための親要素を取得
-  const stars = document.querySelector(".stars");
-
-  // 星を生成する関数
-  const createStar = () => {
-    const starEl = document.createElement("span");
-    starEl.className = "star";
-    const minSize = 1; // 星の最小サイズを指定
-    const maxSize = 2; // 星の最大サイズを指定
-    const size = Math.random() * (maxSize - minSize) + minSize;
-    starEl.style.width = `${size}px`;
-    starEl.style.height = `${size}px`;
-    starEl.style.left = `${Math.random() * 100}%`;
-    starEl.style.top = `${Math.random() * 100}%`;
-    starEl.style.animationDelay = `${Math.random() * 10}s`;
-    stars.appendChild(starEl);
-  };
-
-  // for文で星を生成する関数を指定した回数呼び出す
-  for (let i = 0; i <= 500; i++) {
-    createStar();
+function init(){
+  //stars
+  var style = ["style1", "style2", "style3", "style4"];
+  var tam = ["tam1", "tam1", "tam1", "tam2", "tam3"];
+  var opacity = ["opacity1", "opacity1", "opacity1", "opacity2", "opacity2", "opacity3"];
+  function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
   }
-});
+  var star = "";
+  var numStars = 200;
+  var starry_sky = document.querySelector(".constellation");
+  var widthWindow = window.innerWidth;
+  var heightWindow = window.innerHeight;
+  for (var i = 0; i < numStars; i++) {
+    star += "<span class='star " + style[getRandomArbitrary(0, 4)] + " " + opacity[getRandomArbitrary(0, 6)] + " "
+    + tam[getRandomArbitrary(0, 5)] + "' style='animation-delay: ." +getRandomArbitrary(0, 9)+ "s; left: "
+    + getRandomArbitrary(0, widthWindow) + "px; top: " + getRandomArbitrary(0, heightWindow) + "px;'></span>";
+  }
+  starry_sky.innerHTML = star;
+  }
+window.onload = init;
